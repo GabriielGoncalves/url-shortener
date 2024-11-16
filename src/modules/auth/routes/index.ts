e749074f36b2441db8ea1bd0b7dd8638c00a2447
GabriielGoncalves <novais_gabriel@outlook.com>
2024-11-16 17:59:29 -0300

    feat: create register route

---
+++ src/modules/auth/routes/index.ts
@@ -0,0 +1,34 @@
+import { Router, Request, Response, NextFunction } from "express";
+import authValidator from "../validators";
+import RegisterController from "../controllers/register";
+import { IRegisterData } from "../interfaces/register";
+import registerUserService from "../services/register";
+
+const router = Router();
+
+router.post(
+  "/register",
+  authValidator,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data: IRegisterData = {
+        request: req,
+        body: req.body,
+      };
+
+      const instance = new RegisterController(data, registerUserService);
+
+      const response = await instance.execute();
+
+      return res.status(201).json({
+        msg: response,
+      });
+    } catch (error: any) {
+      next(error);
+    }
+  }
+);
+
+const authRoutes = router;
+
+export default authRoutes;

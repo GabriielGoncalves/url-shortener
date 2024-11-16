2902582350ac5e02f224e0054bd3f863574875b3
GabriielGoncalves <novais_gabriel@outlook.com>
2024-11-16 15:56:24 -0300

    fix: connects to the db during application initiliazation

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,33 @@
-import "dotenv/config"
-import express from 'express';
-import bodyParser from 'body-parser';
-import router from './routes';
-
+import "dotenv/config";
+import express from "express";
+import router from "./routes";
+import connection from "./database/connection";
 
 export default class App {
-    protected app: express.Application;
+  protected app: express.Application;
 
-    constructor() {
-        this.app = express();
-        this.config();
-        this.start();
-    }
+  constructor() {
+    this.app = express();
+    this.config();
+    this.start();
+  }
 
-    protected config() {
-        this.app.use(bodyParser.urlencoded({ extended: true }));
-        this.app.use(express.json());
-        this.app.use('api/', router);
-    }
+  protected config() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use("/api", router);
+  }
 
-    protected start() {
-        const port = process.env.PORT || 3000;
+  protected async start() {
+    const port = process.env.PORT || 3000;
+
+    await connection
+      .initialize()
+      .then(() => {
         this.app.listen(port, () => {
-            console.log(`running on port ${port}`)
-        })
-    }
+          console.log(`running on port ${port}`);
+        });
+      })
+      .catch((e) => console.log(e));
+  }
 }

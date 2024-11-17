bde2792658240efab73d8546a0083e657deef2a1
GabriielGoncalves <novais_gabriel@outlook.com>
2024-11-17 14:15:45 -0300

    fix: add route

--- src/modules/auth/routes/index.ts
+++ src/modules/auth/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response, NextFunction } from "express";
 import authValidator from "../validators";
 import RegisterController from "../controllers/register";
+import LoginController from "../controllers/login";
 import { IRegisterData } from "../interfaces/register";
 import registerUserService from "../services/register";
 
@@ -17,6 +18,29 @@ router.post(
       };
 
       const instance = new RegisterController(data, registerUserService);
+
+      const response = await instance.execute();
+
+      return res.status(201).json({
+        msg: response,
+      });
+    } catch (error: any) {
+      next(error);
+    }
+  }
+);
+
+router.post(
+  "/login",
+  authValidator,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data: IRegisterData = {
+        request: req,
+        body: req.body,
+      };
+
+      const instance = new LoginController(data, {} as any);
 
       const response = await instance.execute();
 

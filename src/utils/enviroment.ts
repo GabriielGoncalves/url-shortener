7986a41f149e01fc3f85e09ffa66d52c714ca5a8
GabriielGoncalves <novais_gabriel@outlook.com>
2024-11-17 19:48:17 -0300

    fix: add variable

--- src/utils/enviroment.ts
+++ src/utils/enviroment.ts
@@ -1,8 +1,9 @@
-import "dotenv/config"
+import "dotenv/config";
 
 const environment = {
-    salt: Number(process.env.CRYPT_SALT! || 10),
-    secret_key: process.env.SECRET_KEY
-}
+  salt: Number(process.env.CRYPT_SALT! || 10),
+  secret_key: process.env.SECRET_KEY,
+  base_url: `${process.env.BASE_URL}:${process.env.PORT}/api`,
+};
 
 export default environment;

2e3476e601b89493031c31e093401031e3cb9098
GabriielGoncalves <novais_gabriel@outlook.com>
2024-11-17 11:29:36 -0300

    fix: import

--- src/common/controller/index.ts
+++ src/common/controller/index.ts
@@ -1,19 +1,20 @@
 import { validationResult } from "express-validator";
-import { IController, IRegisterData } from "../../modules/auth/interfaces/register";
+import { IRegisterData } from "../../modules/auth/interfaces/register";
+import IController from "../interfaces";
 
 export default abstract class BaseController implements IController {
-    constructor(protected data: IRegisterData) {}
-  
-    abstract execute(): Promise<any>;
-  
-    protected validateRequest() {
-      const result = validationResult(this.data.request);
-  
-      const errors = result.array();
-  
-      if (errors.length) {
-        console.log(errors);
-        throw new Error("Errors de validação");
-      }
+  constructor(protected data: IRegisterData) {}
+
+  abstract execute(): Promise<any>;
+
+  protected validateRequest() {
+    const result = validationResult(this.data.request);
+
+    const errors = result.array();
+
+    if (errors.length) {
+      console.log(errors);
+      throw new Error("Errors de validação");
     }
   }
+}

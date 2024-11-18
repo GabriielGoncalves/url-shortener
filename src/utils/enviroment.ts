0c0c4064f47337fbbb5b2c4df7f8e4d1ade1b406
GabriielGoncalves <novais_gabriel@outlook.com>
2024-11-17 21:16:28 -0300

    fix: fix base_url

--- src/utils/enviroment.ts
+++ src/utils/enviroment.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 const environment = {
   salt: Number(process.env.CRYPT_SALT! || 10),
   secret_key: process.env.SECRET_KEY,
-  base_url: `${process.env.BASE_URL}:${process.env.PORT}/api`,
+  base_url: `${process.env.BASE_URL}:${process.env.PORT}/api/redirector`,
 };
 
 export default environment;
